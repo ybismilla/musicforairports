445becd1f14d0b021d32908e9094cbfd4528c8d3
tinyengineer <61503322+ybismilla@users.noreply.github.com>
2021-07-06 21:22:12 -0400

    Finishing this section of the tutorial
    I'm tired of struggling
    
    to tone.js

--- musicforairports.js
+++ musicforairports.js
@@ -12,13 +12,24 @@ const SAMPLE_LIBRARY = {
            { note: 'F',  octave: 4, file: 'samples/viola/F4-tremolo-PB-loop.wav' },
            { note: 'F',  octave: 5, file: 'samples/viola/F5-tremolo-PB-loop.wav' },
            { note: 'F',  octave: 6, file: 'samples/viola/F6-tremolo-PB-loop.wav' }
+    ],
+
+    'celesta': [    
+        { note: 'C',  octave: 4, file: 'samples/viola/c4-hard-PB.wav' },
+        { note: 'C',  octave: 6, file: 'samples/viola/c6-hard-PB.wav' },
+        { note: 'C',  octave: 7, file: 'samples/viola/c7-hard-PB.wav' },
+        { note: 'E',  octave: 5, file: 'samples/viola/E5-hard-PB.wav' },
+        { note: 'E',  octave: 6, file: 'samples/viola/E6-hard-PB.wav' },
+        { note: 'E',  octave: 7, file: 'samples/viola/E7-hard-PB.wav' },
+        { note: 'G',  octave: 5, file: 'samples/viola/G5-hard-PB.wav' },
+        { note: 'G',  octave: 6, file: 'samples/viola/G6-hard-PB.wav' },
+        { note: 'G',  octave: 7, file: 'samples/viola/G7-hard-PB.wav' }
     ]
 }   
 
 const OCTAVE = ['C', 'C3', 'D', 'D#', 'E', 'F' , 'F#', 'G', 'G#', 'A', 'A#', 'B', 'B#'];
 
 let audioContext = new AudioContext();
-
 
 function flatToSharp(note) {
     switch (note) {
@@ -29,7 +40,7 @@ function flatToSharp(note) {
       case 'Ab': return 'G#';
       default:   return note;
     }
-  }
+ }
 
 function getSample(instrument, noteAndOctave) {
     let [, requestedNote, requestedOctave] = /^(\w[b\#]?)(\d)$/.exec(noteAndOctave);
@@ -45,15 +56,13 @@ function getSample(instrument, noteAndOctave) {
     }));
 }
 
-
-
 function noteValue(note, octave) {
     return octave * 12 + OCTAVE.indexOf(note);
 }
 
 function getNoteDistance(note1, octave1, note2, octave2) {
     return noteValue(note1, octave1) - noteValue(note2, octave2);
-  }
+}
 
 function getNearestSample(sampleBank, note, octave) {
   let sortedBank = sampleBank.slice().sort((sampleA, sampleB) => {
@@ -72,23 +81,32 @@ function fetchSample(path) {
         .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer));
 }
 
-
-
-function playSample(instrument, note) {
+function playSample(instrument, note, destination, delaySeconds = 0) {
     getSample(instrument, note).then(({audioBuffer, distance}) => {
         let playbackRate = Math.pow(2, distance / 12);
         let bufferSource = audioContext.createBufferSource();
         bufferSource.buffer = audioBuffer;
+    
         bufferSource.playbackRate.value = playbackRate;
-        bufferSource.connect(audioContext.destination);
-        bufferSource.start();
+        bufferSource.connect(destination);
+        bufferSource.start(audioContext.currentTime + delaySeconds);
       });
 }
 
-setTimeout(() => playSample('viola', 'F4'),  1000);
-setTimeout(() => playSample('viola', 'Ab4'), 2000);
-setTimeout(() => playSample('viola', 'C5'),  3000);
-setTimeout(() => playSample('viola', 'Db5'), 4000);
-setTimeout(() => playSample('viola', 'Eb5'), 5000);
-setTimeout(() => playSample('viola', 'F5'),  6000);
-setTimeout(() => playSample('viola', 'Ab5'), 7000);
+function startLoop(instrument, note, destination, loopLengthSeconds, delaySeconds = 0) {
+    playSample(instrument, note, destination, delaySeconds)
+    setInterval(
+        () => playSample(instrument, note,  destination, delaySeconds, loopLengthSeconds * 1000)
+    );
+}
+
+//startLoop('viola', 'C4', 8, 2);
+
+fetchSample('samples/reverb/AirportTerminal.wav').then(convolverBuffer => {
+
+    let convolver = audioContext.createConvolver();
+    convolver.buffer = convolverBuffer;
+    convolver.connect(audioContext.destination);
+  
+    //startLoop('celesta', 'C4', convolver, 20, 5);
+  });
